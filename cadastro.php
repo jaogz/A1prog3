0f14d087b66e750cee0e49e544a36f3678e69697
João Gabriel Zangalli(JORGEVASCO) <166075318+jaogz@users.noreply.github.com>
2025-04-15 20:12:51 -0300

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>Cadastro</h2>
+<form action="processa_cadastro.php" method="POST">
+  Nome: <input type="text" name="nome"><br>
+  Email: <input type="email" name="email"><br>
+  Senha: <input type="password" name="senha"><br>
+  <input type="submit" value="Cadastrar">
+</form>
+</body>
+</html>
---
+++ logout.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'classes/Sessao.php';
+Sessao::iniciar();
+Sessao::destruir();
+setcookie("email", "", time() - 3600);
+header("Location: login.php");
